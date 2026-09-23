181f836316dc2632b2fc30d4634fc1868840a881
evgeniyaagzholova-commits <evgeniyaagzholova@gmail.com>
2026-09-23 18:25:19 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.enums import ChatType
 from aiogram.client.default import DefaultBotProperties
 
 # ====== НАСТРОЙКИ ======
-BOT_TOKEN = "TOKEN_PLACEHOLDER"
+BOT_TOKEN = "8873918257:AAH3yotGoorAuUnhBp3QLhPwA_gxglokmbg"
 ADMIN_ID = 922545502
 GROUP_ID = -1004291975368
 
